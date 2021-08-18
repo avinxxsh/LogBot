4ed34f6c8de174361b98c5009d8b598d76ed2266
avinxxsh <52997831+avinxxsh@users.noreply.github.com>
2021-08-18 23:16:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord 
 import os
+import random
 from datetime import datetime
 from pytz import timezone
 from keep_alive import keep_alive
@@ -21,30 +22,38 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
+  id = message.guild.id
   log = ('message from {0.author}: {0.content}'.format(message))
-  file1 = open('log.txt', 'a')
+  file1 = open(f'{id}.txt', 'a')
   file1.write('\n' + dt_string + ': ' + log)
   file1.close()
   await bot.process_commands(message)
 
+@bot.command(pass_context=True)
+async def serverid(ctx):
+  id = ctx.message.guild.id
+  await ctx.send(id)
+
 @bot.command()
 async def help(ctx):
-  await ctx.send('LogBot\'s main functionality is maintaining text logs of a server. It is still under development.\n\n**Logbot Commands:**\n**test**: Check if bot is online and functioning as expected\n**rp**: Make LogBot repeat whatever you say\n**printlog**: Print all logs maintained by LogBot *(note that only admins can run this command)*' )
+  await ctx.send('```fix\nLogBot\'s main functionality is maintaining text logs of a server. It is still under development.\n\nLogbot Commands:\ntest: Check if bot is online and functioning as expected\nrp: Make LogBot repeat whatever you say\nprintlog: Print all logs maintained by LogBot (manage server perms required to run this command)\nmembers: Get a list of all server members along with their User ID in a text file (manage server perms required to run this command)\ncrystalball: ask a question and let LogBot answer```' )
 
 @bot.command()
 async def test(ctx):
-  await ctx.send('bot functionality is normal')
+  await ctx.send('LogBot is functioning normally')
 
 @bot.command(pass_context = True)
 @commands.has_permissions(manage_guild = True)
 async def printlog(ctx):
- await ctx.send(file=discord.File('log.txt'))
+  id = ctx.message.guild.id
+  await ctx.send(file=discord.File(f'{id}.txt'))
 
 @bot.command()
 async def rp(ctx, *, arg):
   await ctx.send(arg)
 
-@bot.command()
+@bot.command(pass_context = True)
+@commands.has_permissions(manage_guild = True)
 async def members(ctx):
     with open('users.txt','w') as f:
         async for member in ctx.guild.fetch_members(limit=None):
@@ -53,8 +62,10 @@ async def members(ctx):
     await ctx.send(file=discord.File('users.txt'))
     open('users.txt', 'w').close()
 
-
-
+@bot.command()
+async def crystalball(ctx, *, arg):
+  choice = ['yes','no','maybe','for sure','um.. no?']
+  await ctx.send(random.choice(choice))
 
 keep_alive()
 bot.run(os.getenv('token'))
