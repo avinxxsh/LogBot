a162bc9268d47028c8c46518fff6e8907b1a8d6d
avinxxsh <52997831+avinxxsh@users.noreply.github.com>
2021-08-24 01:30:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,26 @@ bot = commands.Bot('.', intents=intents)
 
 bot.remove_command('help')
 
+class OwnerCommands(commands.Cog):
+
+    def __init__(self, bot):
+        self.client = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("OwnerCommands Is Ready")
+
+    @commands.command()
+    async def servers(self, ctx):
+        activeservers = bot.guilds
+        for guild in activeservers:
+            await ctx.send(guild.name)
+            print(guild.name)
+
+def setup(bot):
+    bot.add_cog(OwnerCommands(bot))
+
+    
 @bot.event
 async def on_ready():
   print('Logged in as: {0.user}'.format(bot))
@@ -31,6 +51,10 @@ async def on_message(message):
   file1.close()
   await bot.process_commands(message)
   
+  #bot replies to mentions
+  if bot.user.mentioned_in(message):
+    await message.channel.send('```yaml\ntype \".help\" for more information```')
+  
   #if message.content =='.testid':
   #  id = message.guild.id
   #  print(id)
@@ -43,7 +67,7 @@ async def serverid(ctx):
 
 @bot.command()
 async def help(ctx):
-  await ctx.send('```yaml\nLogBot\'s main functionality is maintaining text logs of a server. It is still under development.\n\nLogbot Commands:\nUtility:\n- test: Check if bot is online and functioning as expected\n- printlog: Print all logs maintained by LogBot (manage server perms required to run this command)\n- clearlog: Clear all logs maintained by LogBot (administrator perms required to run this command)\n- members: Get a list of all server members along with their User ID in a text file (manage server perms required to run this command)\n- serverid: Get the ID of current Guild/Server\n- update: Get information on the last LogBot update and it\'s features\n\nMisc:\n- rp: Make LogBot repeat whatever you say\n- crystalball: ask a question and let LogBot answer```' )
+  await ctx.send('```yaml\nLogBot\'s main functionality is maintaining text logs of a server. It is still under development. [prefix is \'.\' by default]\n\nLogbot Commands:\nUtility:\n- test: Check if bot is online and functioning as expected\n- printlog: Print all logs maintained by LogBot (manage server perms required to run this command)\n- clearlog: Clear all logs maintained by LogBot (administrator perms required to run this command)\n- members: Get a list of all server members along with their User ID in a text file (manage server perms required to run this command)\n- serverid: Get the ID of current Guild/Server\n- update: Get information on the last LogBot update and it\'s features\n\nMisc:\n- rp: Make LogBot repeat whatever you say\n- crystalball: ask a yes/no question and let LogBot answer```' )
 
 @bot.command()
 async def update(ctx):
@@ -128,11 +152,24 @@ async def error_members(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('Pong! **{0}ms**'.format(round(bot.latency * 1000, 1)))
+
+#@bot.command()
+#async def servercount(ctx):
+#  guilds = list(self.client.guilds)
     
 @bot.command()
 async def crystalball(ctx, *, arg):
   choice = ['yes','no','maybe','for sure','um.. no?']
   await ctx.send(random.choice(choice))
+
+@crystalball.error
+async def error_cb(ctx, error):
+  message = f'```yaml\nERROR: {error}```'
+  await ctx.send(message)
+
+@bot.command()
+async def vote(ctx):
+  await ctx.send('Vote for LogBot at: https://top.gg/bot/877517529520668702/vote/')
 
 keep_alive()
 bot.run(os.getenv('token'))
